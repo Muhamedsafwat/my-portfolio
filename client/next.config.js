9bc735563e7bc66bd8ba5f14b3169d8fcd31abcc
Muhamedsafwat <musafwat666@gmail.com>
2024-11-05 00:07:18 +0200

    create server branch

--- client/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["trydo.rainbowit.net", "res.cloudinary.com"],
-  },
-};
-
-module.exports = nextConfig;
--- client/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as NavBar } from "./NavBar";
-export { default as Footer } from "./Footer";
-export { default as ScrollToTop } from "./ScrollToTopButton";
-export { default as Gallery } from "./ProjectGallery";
-export { default as ContactForm } from "./ContactForm";
